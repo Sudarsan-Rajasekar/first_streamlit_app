ffa3913ef24ba3eb8a4de54a453cab36193421bf
Sudarsan Rajasekar <107539573+Sudarsan-Rajasekar@users.noreply.github.com>
2022-06-15 17:18:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 df = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-df.reset_index('Fruit')
+df.set_index('Fruit')
 
 # list for the users to pick from 
 streamlit.multiselect("Pick from the list:",list(df.index))
