82e2dadd4099a8c9d574b98ab9c0b4cbf4ecca6a
Sudarsan Rajasekar <107539573+Sudarsan-Rajasekar@users.noreply.github.com>
2022-06-15 17:05:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title("My Parents new healthy dinner")
-streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 and Blueberry Oat Meal")
-streamlit.text("Kale, Spinach and Rocket smoothie")
-streamlit.text("Hard Boiled - Free Range egg")
+streamlit.header("Breakfast Favorites")
+streamlit.text("🥗 Omega 3 and Blueberry Oat Meal")
+streamlit.text("🧃Kale, Spinach and Rocket smoothie")
+streamlit.text("🥚 Hard Boiled - Free Range egg")
+streamlit.text("🍞🥑Avocado Toast")
