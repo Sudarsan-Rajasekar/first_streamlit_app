3776e6b07b0b201d7523b79c2b55d515d63eb789
Sudarsan Rajasekar <107539573+Sudarsan-Rajasekar@users.noreply.github.com>
2022-06-15 17:08:31 +0530

    Update streamlit_app.py
    Include data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas 
+
 streamlit.title("My Parents new healthy dinner")
 streamlit.header("Breakfast Favorites")
 streamlit.text("🥗 Omega 3 and Blueberry Oat Meal")
@@ -7,3 +9,7 @@ streamlit.text("🥚 Hard Boiled - Free Range egg")
 streamlit.text("🍞🥑 Avocado Toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+df = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(df)
+
