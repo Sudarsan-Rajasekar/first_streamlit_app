c3a99f3539929d5d703424241be1cbc915a77e20
Sudarsan Rajasekar <107539573+Sudarsan-Rajasekar@users.noreply.github.com>
2023-10-31 20:41:09 +0530

    Add files via upload

--- Sentiment.py
+++ Sentiment.py
@@ -1,17 +1,25 @@
 import streamlit as st
 from transformers import pipeline 
 
+st.header('Sentimental Analysis 😊😟')
+
 @st.cache_data
 def get_model():
     return pipeline("sentiment-analysis")
 
 classifier = get_model()
 
-vSentence = st.text_input('Enter sentimental text here...')
-
+vSentence = st.text_input('Enter text to predict sentiment...')
+st.write(vSentence)
 if vSentence is not None or vSentence == '':
     result = classifier(vSentence)[0]
     if result['label'] == 'POSITIVE':
         st.success(f"😀 Positive Sentence.. ")
     else:
         st.error(f"😟 Negative Sentence..")
+
+st.write("""
+**Examples:**\n
+I love this product. It's amazing!\n
+This movie is terrible. I can't stand it.
+""")
